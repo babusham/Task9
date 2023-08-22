ddfbc616a04d105c1756c7cbfe04eb62248b07d2
Babusham <108461730+babusham@users.noreply.github.com>
2023-08-23 00:50:50 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section
+      className=" d-flex  flex-column  justify-content-center  align-items-center "
+      id="about"
+    >
+      <div className=" text-center ">
+        <h1>Welcome <br></br>To <br></br>About page</h1>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section
+      className=" d-flex  flex-column  justify-content-center  align-items-center "
+      id="contact"
+    >
+      <div className=" text-center ">
+        <h1>Welcome <br></br>To<br></br> Contact page</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
